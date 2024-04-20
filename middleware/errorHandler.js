1945bab035e7c83b22366255006387d8816ac9c4
Adewale Kujore <Adewalekujore6@gmail.com>
2024-04-20 15:51:45 +0100

    finished

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -4,7 +4,7 @@ import { logger } from '../utils/config.js';
 const errorHandler = (error, req, res, next) => {
     logger.error(error);
     if (error.name === 'CastError') {
-        return res.status(400).send({ error: 'malformatted id' });
+        return res.status(400).send({ error: 'Invalid id' });
     } else if (error.name === 'ValidationError') {
         return res.status(400).json({ error: 'Invalid Email' });
     } else if (error.name === 'JsonWebTokenError') {
